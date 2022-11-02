db3e346fd37340402150cd27278d199e509d28b0
Deepika Velapure <velapure@pdx.edu>
2022-11-02 00:30:09 -0700

    Updated changes for form.js to use node:querystring

--- hw3/03-form.js
+++ hw3/03-form.js
@@ -1,50 +1,37 @@
 /** @format */
 
 const http = require("http");
+const static = require("node-static");
+const querystring = require("node:querystring");
 const port = process.env.PORT || 5001;
 
 // http://localhost:5001/form should return a form with input elements for username, email, and submit button
 
 // http://localhost:5001/submit should return all the data the user entered
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/form") {
-    const postHTML = `<html><body>
-    <form method="post">
-    <label for="username">Username: </label>
-    <input name="username" /><br /><br />
-    <label for="email">Email: </label>
-    <input type="email" name="email" /><br /><br />
-    <input type="submit" value="Submit" /></form>
-    </body></html>`;
+const file = new static.Server("./public");
 
-    req.on("data", (formDetails) => {
-      console.log(formDetails);
-      res.writeHead(302, { Location: "/submit", "Set-Cookie": `formData=${formDetails}` });
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/form") {
+    file.serveFile("/form.html", 200, {}, req, res);
+  } else if (req.method === "POST" && req.url === "/submit") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      let formDetails = "";
+      let userData = querystring.parse(body);
+      console.log(body);
+      console.log(userData);
+      const { username, email } = userData;
+      formDetails = formDetails.concat(`<b>Name: `).concat(`${username}`).concat(`<br><br>`);
+      formDetails = formDetails.concat(`<b>Email: `).concat(`${email}`);
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`${formDetails}`);
       res.end();
     });
-
-    req.on("end", () => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(postHTML);
-    });
-  } else if (req.url === "/submit") {
-    let formDetails = "";
-    let allCookiesArray = req.headers.cookie.split(";");
-    allCookiesArray.forEach((cookie) => {
-      if (cookie.includes("formData")) {
-        let cookieArray = cookie.split("=");
-        let uName = "Name";
-        let uNameValue = cookieArray[2].split("&")[0];
-        let uEmail = "Email";
-        let uEmailValue = cookieArray[3].replace("%40", "@");
-        formDetails = formDetails.concat(`<b>${uName}: `).concat(`${uNameValue}`).concat(`<br><br>`);
-        formDetails = formDetails.concat(`<b>${uEmail}: `).concat(`${uEmailValue}`);
-      }
-    });
-
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(`${formDetails}`);
   }
 });
 
