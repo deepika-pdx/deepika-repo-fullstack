5e1e80b9f698dca44dd3ecaf5e1c73902c635b3d
Deepika Velapure <velapure@pdx.edu>
2022-11-03 16:31:39 -0700

    Commit changes as per review comment

--- hw3/03-form.js
+++ hw3/03-form.js
@@ -24,9 +24,21 @@ const server = http.createServer((req, res) => {
       let userData = querystring.parse(body);
       console.log(body);
       console.log(userData);
-      const { username, email } = userData;
+      const { username, email, comments, newsletterCheck } = userData;
       formDetails = formDetails.concat(`<b>Name: `).concat(`${username}`).concat(`<br><br>`);
-      formDetails = formDetails.concat(`<b>Email: `).concat(`${email}`);
+      formDetails = formDetails.concat(`<b>Email: `).concat(`${email}`).concat(`<br><br>`);
+
+      if (comments !== "") {
+        formDetails = formDetails.concat(`<b>Comments: `).concat(`${comments}`).concat(`<br><br>`);
+      } else {
+        formDetails = formDetails.concat(`<b>Comments: `).concat(`n/a`).concat(`<br><br>`);
+      }
+
+      if (newsletterCheck === "") {
+        formDetails = formDetails.concat(`<b>Newsletter: `).concat(`Yes, sign me up for the newsletter.`).concat(`<br><br>`);
+      } else {
+        formDetails = formDetails.concat(`<b>Newsletter: `).concat(`No, thank you.`).concat(`<br><br>`);
+      }
 
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(`${formDetails}`);
